15adadc0d655fca50a997ebf2fd8398cbe8c747c
Johnsgeorge03 <johnsgeorge03@gmail.com>
2023-03-31 21:08:56 -0500

    with target with replay

--- train_discreteaction_pendulum.py
+++ train_discreteaction_pendulum.py
@@ -49,18 +49,20 @@ agent               = Agent(n_observations, n_actions, hidden_size,
                             epsilon_decay, tau, device)
 
 agent.batch_size    = 128
-agent.memory_size   = 128
-agent.update_freq   = 1
-agent.anneal_steps  = 1000
+agent.memory_size   = 10000 # if equal to batch_size implies no replay
+agent.update_freq   = 500 # > 1 implies target network
+agent.anneal_steps  = 10000
 # update freq = 500
-# no replay batch size = 128
+# no replay batch size = one-tenth of total steps(10000)
 
 ## DIRECTORIES
-weight_dir          = 'NN_weights/no_target_no_replay/'
-fig_dir             = 'figures/no_target_no_replay/'
-reward_file         = 'rewards_no_tar_no_rep.txt'
+weight_dir          = 'NN_weights/target_replay/'
+fig_dir             = 'figures/target_replay/'
+reward_dir          = 'rewards/'
+reward_file         = 'rewards.txt'
 os.makedirs(fig_dir, exist_ok=True)
 os.makedirs(weight_dir, exist_ok=True)
+os.makedirs(reward_dir, exist_ok=True)
 
 ## TRAIN
 num_episodes        = 1000
@@ -124,12 +126,16 @@ plt.show()
 ## WRITE REWARDS AND MODEL PARAMS TO FILES
 torch.save(agent.policy_network.state_dict(), weight_dir + 'qnet_model_weights_end.pth')
 torch.save(agent.target_network.state_dict(), weight_dir + 'tnet_model_weights_end.pth')
-with open(reward_file, 'w+') as f:
+with open(reward_dir + reward_file, 'w+') as f:
     for items in all_udissc_r:
         f.write('%s\n' %items)
     print("File written successfully")
 f.close()
 
+# weight_dir          = 'NN_weights/target_replay/'
+# fig_dir             = 'figures/target_replay/'
+# reward_dir          = 'rewards/'
+# reward_file         = 'rewards.txt'
 
 
 ## PLOTS
@@ -139,17 +145,19 @@ plotter.plot_policy(fig_dir + 'policy.png')
 plotter.plot_value_fn(fig_dir + 'value_fn.png')
 plotter.plot_trajectory(fig_dir + 'trajectory.png')
 plotter.generate_video(fig_dir + 'trajectory_video.gif')
-plotter.plot_learning_curve(gamma, reward_file, 
+plotter.plot_learning_curve(gamma, reward_dir + reward_file, 
                             fig_dir + 'undiscounted_learning_curve.png', 
                             fig_dir + 'discounted_learning_curve.png')
 
 legend_list   = ['returns', 'returns (no replay)', 'returns (no target)',
                   'returns (no replay, no target)']
-file_list     = ['rewards.txt', 'rewards_no_rep.txt', 'rewards_no_tar.txt',
-                  'rewards_no_tar_no_rep.txt']
+file_list     = [reward_dir + 'rewards.txt', reward_dir + 'rewards_no_rep.txt', 
+                 reward_dir + 'rewards_no_tar.txt',
+                 reward_dir + 'rewards_no_tar_no_rep.txt']
 
 plotter.plot_ablation_study(gamma, file_list, legend_list, 
-                            'figures/ablation_study.png')
+                            'figures/ablation_study.png',
+                            'figures/ablation_study_bar.png')
 
 
 # if __name__ == '__main__':
